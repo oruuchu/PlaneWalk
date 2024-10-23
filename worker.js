97a0724af54d43abc306a7cee4965d4edd617c64
折る宇宙 <yokoig0725@gmail.com>
2024-10-23 16:45:41 +0900

    Update worker.js

--- worker.js
+++ worker.js
@@ -10,7 +10,8 @@ const assets = [
   'https://api.mapbox.com/mapbox.js/plugins/leaflet-fullscreen/v1.0.1/Leaflet.fullscreen.min.js',
   'https://api.mapbox.com/mapbox.js/plugins/leaflet-fullscreen/v1.0.1/leaflet.fullscreen.css',
   "https://cdn.jsdelivr.net/npm/leaflet-easybutton@2.4.0/src/easy-button.min.js",
-  "https://cdn.jsdelivr.net/npm/leaflet-easybutton@2.4.0/src/easy-button.min.css"
+  "https://cdn.jsdelivr.net/npm/leaflet-easybutton@2.4.0/src/easy-button.min.css",
+  'http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png'
 ];
 
 /**
