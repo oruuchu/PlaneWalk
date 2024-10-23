f7eeebde4bb2c14ff33c1f3873afe2c46024ffbc
折る宇宙 <yokoig0725@gmail.com>
2024-10-23 16:18:32 +0900

    Update worker.js

--- worker.js
+++ worker.js
@@ -5,12 +5,7 @@ const CACHE_NAME = APP_NAME + "_" + VERSION;
 const assets = [
   "/",
   "/index.html",
-  "/192.png"/*,
-  "https://unpkg.com/leaflet@1.3.1/dist/leaflet.js",
-  'https://api.mapbox.com/mapbox.js/plugins/leaflet-fullscreen/v1.0.1/Leaflet.fullscreen.min.js',
-  'https://api.mapbox.com/mapbox.js/plugins/leaflet-fullscreen/v1.0.1/leaflet.fullscreen.css',
-  "https://cdn.jsdelivr.net/npm/leaflet-easybutton@2.4.0/src/easy-button.min.js",
-  "https://cdn.jsdelivr.net/npm/leaflet-easybutton@2.4.0/src/easy-button.min.css"*/
+  "/192.png"
 ];
 
 /**
