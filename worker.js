f4a3fe622759e93f2db31f1ceaa33100a835bc2c
折る宇宙 <yokoig0725@gmail.com>
2024-10-26 21:22:06 +0900

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,5 +1,5 @@
 const APP_NAME = "SolarSystemWalk";
-const VERSION = "2.0.0"; 
+const VERSION = "2.0.1"; 
 const CACHE_NAME = APP_NAME + "_" + VERSION;
 
 const assets = [
@@ -10,8 +10,7 @@ const assets = [
   'https://api.mapbox.com/mapbox.js/plugins/leaflet-fullscreen/v1.0.1/Leaflet.fullscreen.min.js',
   'https://api.mapbox.com/mapbox.js/plugins/leaflet-fullscreen/v1.0.1/leaflet.fullscreen.css',
   "https://cdn.jsdelivr.net/npm/leaflet-easybutton@2.4.0/src/easy-button.min.js",
-  "https://cdn.jsdelivr.net/npm/leaflet-easybutton@2.4.0/src/easy-button.min.css",
-  'http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png'
+  "https://cdn.jsdelivr.net/npm/leaflet-easybutton@2.4.0/src/easy-button.min.css"
 ];
 
 /**
