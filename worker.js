c13a91d39c0347b2428a13c1d24661109e7cc422
折る宇宙 <yokoig0725@gmail.com>
2024-10-30 22:34:21 +0900

    Update worker.js

--- worker.js
+++ worker.js
@@ -6,7 +6,7 @@ const assets = [
   "./",
   "./index.html",
   "./192.png",
-  "./IMG_1948.JPG"
+  "./IMG_1948.JPG",
   "https://unpkg.com/leaflet@1.3.1/dist/leaflet.js",
   'https://api.mapbox.com/mapbox.js/plugins/leaflet-fullscreen/v1.0.1/Leaflet.fullscreen.min.js',
   'https://api.mapbox.com/mapbox.js/plugins/leaflet-fullscreen/v1.0.1/leaflet.fullscreen.css',
